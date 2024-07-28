8d183d80e6767135ca5cbbb65e66793c6cc85474
Kai Boon Ee <eekaiboon@gmail.com>
2024-07-28 10:30:15 -0700

    phonebook step 5

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    response.json(persons)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
-  
+
     response.status(204).end()
-  })
+})
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
@@ -45,10 +46,18 @@ app.get('/api/persons/:id', (request, response) => {
 
     if (person) {
         response.json(person)
-      } else {
+    } else {
         response.status(404).end()
-      }
-  })
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    const personId = getRandomInt(10000)
+    person.id = String(personId)
+    persons = persons.concat(person)
+    response.json(person)
+})
 
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people<br/>${new Date()}`)
@@ -56,5 +65,10 @@ app.get('/info', (request, response) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+  
