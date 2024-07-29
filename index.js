f451ebba6e7b12a7cdd7225a88c1393c7d19670d
Kai Boon Ee <eekaiboon@gmail.com>
2024-07-28 21:28:41 -0700

    phonebook step 9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('request', function (req, res) {
     return JSON.stringify(req.body)
@@ -8,6 +9,7 @@ morgan.token('request', function (req, res) {
 const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request'))
+app.use(cors())
 
 let persons = [
     {
